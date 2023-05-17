487d164f20d2bd99109085ef537d48b82f0ad715
Jacket777 <hxqsu37@sina.com>
2023-05-17 11:29:03 +0800

    竞态条件实例

---
+++ thread_basic/src/main/java/com/thread/basic01/c13/T02_CounterThread.java
@@ -0,0 +1,30 @@
+package com.thread.basic01.c13;
+
+/**
+ * 15-2 race condition exemple
+ * 竞态条件实例
+ */
+public class T02_CounterThread extends Thread{
+    private static int counter = 0;
+    @Override
+    public void run(){
+        for (int i = 0; i < 1000; i++) {
+            counter++; //它不是原子操作
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        int num = 1000;
+        Thread[]threads = new Thread[num];
+        for (int i = 0; i < num; i++) {
+            threads[i] = new T02_CounterThread();
+            threads[i].start();
+        }
+
+        for (int i = 0; i < num; i++) {
+            threads[i].join();
+        }
+        System.out.println(counter);
+    }
+
+}
