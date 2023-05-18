a03b6232bef59f8c33393a142b86af4b78c45723
Jacket777 <hxqsu37@sina.com>
2023-05-18 09:53:50 +0800

    简单协作实例--wait与notify

---
+++ thread_basic/src/main/java/com/thread/basic01/c31/T01_WaitThread.java
@@ -0,0 +1,37 @@
+package com.thread.basic01.c31;
+
+/**
+ * 15-12 简单协作实例
+ * notify  wait方法只能在synchronized代码内部被调用
+ * 如果没有则抛出IllegalMonitorStateException
+ */
+public class T01_WaitThread extends Thread{
+    private volatile boolean fire = false;
+    @Override
+    public void run() {
+        synchronized (this){
+            while(!fire){
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            }
+        }
+
+
+    public synchronized void fire(){
+        this.fire = true;
+        notify();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        T01_WaitThread waitThread = new T01_WaitThread();
+        waitThread.start();
+        Thread.sleep(1000);
+        System.out.println("fire");
+        waitThread.fire();
+    }
+    }
+
